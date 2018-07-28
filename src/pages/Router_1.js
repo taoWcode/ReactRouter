3f8f8525c6629b2bee356b98e9b7ab3452f9fb8a
weitao <itophp@itophp.com>
2018-07-28 12:37:17 +0800

    路由的基本应用

---
+++ src/pages/Router_1.js
@@ -0,0 +1,81 @@
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2018-07-28 11:26:25
+ * @version $Id$
+ */
+/*最基础的路由*/
+
+import React from 'react';
+import {BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+
+const RouterPage1 = ()=>{
+		return (
+			<Router>
+				<div>
+					<ul className="m-nav">
+						<li>
+							<NavLink to="/">首页</NavLink>
+						</li>
+						<li>
+							<NavLink to="/About">关于我们</NavLink>
+						</li>
+						<li>
+							<NavLink to="/Topics">主题</NavLink>
+						</li>
+					</ul>
+					
+					<Route path = '/' exact component ={Home}/>
+					<Route path = '/About' component = {About}/>
+					<Route path = '/Topics' component = {Topics}/>
+				</div>
+			</Router>
+			)
+}
+
+
+const Home = ()=> {
+	return (<div>Home</div>)
+}
+
+const About = ()=>{ 
+	return (<div>About</div>);
+}
+
+const Topics = ({match}) =>{
+	console.log(match);
+	return (<div>
+			<h2>主体列表</h2>
+			
+			<ul>
+				<li>
+					<NavLink to={`${match.url}/rendering`}>使用React渲染</NavLink>
+				</li>
+				<li>
+					<NavLink to={`${match.url}/components`}>组件</NavLink>
+				</li>
+				<li>
+					<NavLink to={`${match.url}/props-v-state`}>属性 v. 状态</NavLink>
+				</li>
+			</ul>
+			
+			<div>
+			<Route path = {`${match.url}/:topicId`} component={Topic}/>
+			
+
+			<Route exact path={match.url} render= { ()=>(<h3>请选择一个主题</h3>)} />
+			
+			
+			</div>
+			
+
+		</div>);
+}			
+
+const Topic =({match})=>{
+	return (<div>
+		<h3>{match.params.topicId}</h3>
+	</div>)
+}
+export default RouterPage1;
+
